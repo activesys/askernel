4f82c0ddee5234830d2a3bed1dd3e1b8ec44f95f
Bo Wang <activesys.wb@gmail.com>
2013-08-14 11:19:02 +0800

    change directory.

--- protected-mode/multi-segment-model/build_protect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# build protect
-as -o protect.o protect.s
-ld -Ttext 0x7c00 -o protect protect.o
-objcopy -O binary protect protect.bin
-dd if=protect.bin of=protect.img count=1
-chmod -x protect.img
-rm -f protect protect.o protect.bin
