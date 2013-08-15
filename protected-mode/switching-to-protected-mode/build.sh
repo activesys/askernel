157934782da7cd01c7e602dcc1315ad06f059377
Bo Wang <activesys.wb@gmail.com>
2013-08-15 21:21:07 +0800

    delete all contents.

--- protected-mode/switching-to-protected-mode/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# build boot
-as -o boot.o boot.s
-ld -Ttext 0x7c00 -o boot boot.o
-objcopy -O binary boot boot.bin
-dd if=boot.bin of=boot.img count=1
-chmod -x boot.img
-rm -f boot boot.o boot.bin
